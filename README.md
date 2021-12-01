8a4bd3e38b21435d81322770a9da23020b1cd4cd
zhaolandelong <landelong.zhao@tusimple.ai>
2021-12-01 22:38:14 +0800

    feat: add inquirer

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # bazi-name
-根据五格剖象法、八字五行取名
+根据五格剖象法、八字五行取名。文字库见 `chars.md`。
 
 ## 使用
-1. cnpm i
-2. tsc && node dist/main.js
-3. see result.json
-
-## 内置脚本
-- 查看【吉数】 `tsc && node dist/scripts.js`
+1. npm i && npm run build
+2. npm start
+3. see the result
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as inquirer from "inquirer";
 import { CharData, Ele, GBData, Sex } from "./types";
 import { getGoodNums, getNameScore } from "./utils";
 import chars from "./data/chars.json";
@@ -59,18 +60,89 @@ function main(options: Options) {
       }
     });
   });
-
-  fs.writeFileSync("./result.json", JSON.stringify(result, null, 2));
+  return result;
 }
 
-const goodNums = getGoodNums(basicNums, Sex.female);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "surname",
+      message: "姓氏（surname）：",
+      validate(input) {
+        if (/^[\u4e00-\u9fa5]$/.test(input)) {
+          return true;
+        }
+        return new Error("暂时只支持中文单姓");
+      },
+    },
+    {
+      type: "list",
+      name: "sex",
+      message: "性别（sex）：",
+      choices: [
+        { name: "女（female）", value: Sex.female },
+        { name: "男（male）", value: Sex.male },
+      ],
+    },
+    {
+      type: "list",
+      name: "secdEle",
+      message: "中间（第二个）字的属性（The element of middle char）：",
+      choices: [
+        { name: "木（wood）", value: Ele.wood },
+        { name: "火（fire）", value: Ele.fire },
+        { name: "土（earth）", value: Ele.earth },
+        { name: "金（metal）", value: Ele.metal },
+        { name: "水（water）", value: Ele.water },
+      ],
+    },
+    {
+      type: "list",
+      name: "lastEle",
+      message: "最后一个字的属性（The element of last char）：",
+      choices: [
+        { name: "木（wood）", value: Ele.wood },
+        { name: "火（fire）", value: Ele.fire },
+        { name: "土（earth）", value: Ele.earth },
+        { name: "金（metal）", value: Ele.metal },
+        { name: "水（water）", value: Ele.water },
+      ],
+    },
+    {
+      type: "list",
+      name: "minScore",
+      message: "筛选严格程度（Screening criteria）：",
+      default: 1,
+      choices: [
+        { name: "极严（highest）", value: 6 },
+        { name: "严格（high）", value: 4 },
+        { name: "普通（normal）", value: 2 },
+        { name: "低（low）", value: 0 },
+      ],
+    },
+  ])
+  .then((answsers) => {
+    const { surname, sex, secdEle, lastEle, minScore } = answsers;
 
-const options = {
-  charsData: chars,
-  numsData: goodNums,
-  surname: "赵",
-  eles: [Ele.fire, Ele.earth], // tu jin 98, jin tu 98, jin jin 91, tu tu 95,
-  minScore: 4,
-}
+    const goodNums = getGoodNums(basicNums, sex);
 
-main(options);
+    const options = {
+      charsData: chars,
+      numsData: goodNums,
+      surname,
+      eles: [secdEle, lastEle],
+      minScore,
+    };
+
+    const result = main(options);
+
+    const fileName = `./result-${surname}-${Ele[secdEle]}-${Ele[lastEle]}-${Sex[sex]}-${minScore}.json`;
+    
+    console.log(`成功：请查看 ${fileName} 文件（Success: Please watch the file）。`);
+
+    fs.writeFileSync(fileName, JSON.stringify(result, null, 2));
+  })
+  .catch((e) => {
+    console.error(e);
+  });
